3fe06b595154c39205c74fff07f2197b3f5001ae
ssoyeee <busanpsy1@gmail.com>
2026-04-01 17:39:15 -0400

    fix api handler export syntax

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
